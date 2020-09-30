12b2975a7d9cdae87866b9d2204a1da1266d6d3f
Nigar Abdullayeva <nigar_abdullaeva@hotmail.com>
2020-09-29 23:17:25 -0500

    commit

---
+++ my_portfolio/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Mybio from "../components/Mybio";
+import Links from "../components/Links";
+
+export default function() {
+    return (
+        <div>
+        <Mybio/>
+        <Links/>
+        </div>
+    )
+}
